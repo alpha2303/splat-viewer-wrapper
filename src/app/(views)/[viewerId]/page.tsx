d15eddf180c7f648c45972bd6167a1e5f582bc12
Rahul Pavithran <rahul.pavithran99@gmail.com>
2025-04-16 11:46:04 -0400

    [fix] Fixed issue with fullscreen API not working

--- src/app/(views)/[viewerId]/page.tsx
+++ src/app/(views)/[viewerId]/page.tsx
@@ -3,10 +3,10 @@ import { useParams } from "next/navigation";
 import { UUID } from "node:crypto";
 import React, { useEffect } from "react";
 
-import { UrlMappingResponse } from "../../lib/models/UrlMappingResponse";
+import { UrlMappingResponse } from "../../lib/definitions/UrlMappingResponse";
 
 export default function ViewerPage() {
-  const [viewerURL, setViewerURL] = React.useState<URL>(null);
+  const [viewerRoute, setViewerRoute] = React.useState<string>(null);
   const [viewerFailed, setViewerFailed] = React.useState<boolean>(false);
   const viewerId: UUID = useParams<{ viewerId: UUID }>().viewerId;
 
@@ -18,20 +18,25 @@ export default function ViewerPage() {
           throw new Error(`HTTP error! status: ${res.status}`);
         }
         const urlMapping: UrlMappingResponse = await res.json();
-        const viewerURL = new URL(urlMapping.viewer_url);
-        setViewerURL(viewerURL);
+        const viewerPath = `dist/index.html?settings=${urlMapping.settings_url}&content=${urlMapping.content_url}`;
+        setViewerRoute(viewerPath);
       })
       .catch((error) => {
         console.error(error);
         setViewerFailed(true);
       });
-  }, []);
+  });
 
   return (
     <>
-      <div className="viewer-wrapper">
-        {viewerURL && viewerURL !== null ? (
-          <iframe src={viewerURL.toString()} />
+      <div className="flex justify-center items-center overflow-hidden min-h-screen min-w-screen">
+        {viewerRoute && viewerRoute !== null ? (
+          <iframe
+            className="min-h-screen min-w-screen w-full h-full relative"
+            src={viewerRoute}
+            allow="fullscreen"
+            loading="lazy"
+          />
         ) : viewerFailed ? (
           <div>Could not load viewer</div>
         ) : (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <>
-      <div className="default">
+      <div className="flex justify-center items-center min-h-screen min-w-screen">
         <h1>DiffStudio!</h1>
       </div>
     </>
